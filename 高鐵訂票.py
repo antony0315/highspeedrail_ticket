91afcd37ce4f69830f54bc96709a06699bb34a3f
antony0315 <68135399+antony0315@users.noreply.github.com>
2024-07-29 13:45:40 +0800

    Update 高鐵訂票.py

--- 高鐵訂票.py
+++ 高鐵訂票.py
@@ -8,52 +8,60 @@ import time
 import undetected_chromedriver as uc
 
 
-def order_info_flow(ticket_count:str,startStation:str,destination:str,order_date:str,toTime:str):
-    searchTime=str(int(toTime[:2])-2).zfill(2)+':'+'00'
-    driver.get('https://irs.thsrc.com.tw/IMINT/?locale=tw')
+def order_info_flow(ticket_count:str, startStation:str, destination:str, order_date:str, toTime:str):
+    searchTime = str(int(toTime[:2])-2).zfill(2) + ':' + '00'
     try:
         cookie_btn = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[6]/div[3]/div/div/button')))
         cookie_btn.click()
-    except:
-        pass
-    #訂票數量
-    select_element=WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[6]/main/section/div/div/form/div[5]/div/div[1]/div/select')))
-    select_element=Select(select_element)
-    select_element.select_by_visible_text(ticket_count)
-    #出發站
-    select_element=WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[6]/main/section/div/div/form/div[4]/div[1]/div/div[1]/div/select')))
-    select_element=Select(select_element)
-    select_element.select_by_visible_text(startStation)
-    #抵達站
-    select_element=WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[6]/main/section/div/div/form/div[4]/div[1]/div/div[3]/div/select')))
-    select_element=Select(select_element)
-    select_element.select_by_visible_text(destination)
-    #日期
-    js='document.getElementsByClassName("uk-input")[0].removeAttribute("readonly");'
-    driver.execute_script(js)
-    driver.execute_script("document.getElementById('toTimeInputField').value = arguments[0];", order_date)
-    #時間
-    select_element=WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[6]/main/section/div/div/form/div[4]/div[2]/div/div[2]/div[1]/select')))
-    select_element=Select(select_element)
-    select_element.select_by_visible_text(searchTime)
-    with open('captcha.png', 'wb') as f:
-        f.write(WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[6]/main/section/div/div/form/div[6]/div[2]/div/img'))).screenshot_as_png)
-    ocr=ddddocr.DdddOcr()
-    with open('captcha.png', 'rb') as f:
-        img=f.read()
-    res=ocr.classification(img)
-    captcha=WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[6]/main/section/div/div/form/div[6]/div[2]/div/input')))
-    captcha.send_keys(res)
-    start=WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[6]/main/section/div/div/form/input')))
-    start.click()
-    if driver.current_url == "https://irs.thsrc.com.tw/IMINT/?locale=tw":
-        raise ValueError('失敗')
-    return None
+    except Exception as e:
+        print(f"No cookie button found: {e}")
+    try:
+        # 訂票數量
+        select_element = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[6]/main/section/div/div/form/div[5]/div/div[1]/div/select')))
+        select_element = Select(select_element)
+        select_element.select_by_visible_text(ticket_count)
 
+        # 出發站
+        select_element = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[6]/main/section/div/div/form/div[4]/div[1]/div/div[1]/div/select')))
+        select_element = Select(select_element)
+        select_element.select_by_visible_text(startStation)
+
+        # 抵達站
+        select_element = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[6]/main/section/div/div/form/div[4]/div[1]/div/div[3]/div/select')))
+        select_element = Select(select_element)
+        select_element.select_by_visible_text(destination)
+
+        # 日期
+        js = 'document.getElementsByClassName("uk-input")[0].removeAttribute("readonly");'
+        driver.execute_script(js)
+        driver.execute_script("document.getElementById('toTimeInputField').value = arguments[0];", order_date)
+
+        # 時間
+        select_element = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[6]/main/section/div/div/form/div[4]/div[2]/div/div[2]/div[1]/select')))
+        select_element = Select(select_element)
+        select_element.select_by_visible_text(searchTime)
+
+        with open('captcha.png', 'wb') as f:
+            f.write(WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[6]/main/section/div/div/form/div[6]/div[2]/div/img'))).screenshot_as_png)
+        
+        ocr = ddddocr.DdddOcr()
+        with open('captcha.png', 'rb') as f:
+            img = f.read()
+        
+        res = ocr.classification(img)
+        captcha = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[6]/main/section/div/div/form/div[6]/div[2]/div/input')))
+        captcha.send_keys(res)
+        start = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[6]/main/section/div/div/form/input')))
+        start.click()
+        if driver.current_url != "https://irs.thsrc.com.tw/IMINT/?wicket:interface=:1::":
+            raise ValueError('失敗')
+    except Exception as e:
+        print(f"Error during order flow: {e}")
+        raise
 
 def retry_function(func, max_attempts=10, delay=1):
     try:
-        return func
+        func()
     except Exception as e:
         if max_attempts > 1:
             print(f"Attempt failed: {e}. Retrying...")
@@ -62,7 +70,7 @@ def retry_function(func, max_attempts=10, delay=1):
         else:
             print("Max attempts reached. Failed to execute function.")
             raise
-            
+
 
 
 
@@ -103,6 +111,12 @@ def complete_order(orderID:str,phone:str,email:str,toTime:str,*args):
             passsenger_id_input.send_keys(psg_id)
     except:
         pass
+    
+    member_check = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "memberSystemRadio1")))
+    member_check.click()
+    member_check2 = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "memberShipCheckBox")))
+    member_check2.click()
+
     check_box=WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.NAME, "agree")))
     check_box.click()
     
@@ -132,17 +146,17 @@ if __name__=='__main__':
     ticket_count='1'
     startStation="台中"
     destination="台北"
-    order_date="2024/05/23"
+    order_date="2024/07/31"
     toTime="08:40"
     orderID="L123456789"
     psgID="L123456789","L123456789"
     phone="0981064195"
     email="antony10283@gmail.com"
     driver=uc.Chrome()
-    retry_function(order_info_flow(ticket_count,startStation,destination,order_date,toTime),10,1)
+    driver.get('https://irs.thsrc.com.tw/IMINT/?locale=tw')
+    retry_function(lambda: order_info_flow(ticket_count,startStation,destination,order_date,toTime),10,1)
     complete_order(orderID,phone,email,toTime,psgID)
     
     
     
-
 
